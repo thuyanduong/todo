c8cfad5b03564eb40e2a8e624aa11f087eef24f7
CLeeBenjamin <castonboyd@gmail.com>
2023-02-14 13:19:09 -0500

    config updated

--- dbconfig.js
+++ dbconfig.js
@@ -1,12 +1,9 @@
 const { Pool } = require('pg')
 
-const connectionString = 'postgresql://postgres:3YPdLyslfVLPZObDQQND@containers-us-west-198.railway.app:6206/railway'
+const connectionString = 'postgresql://postgres:f1ynjwcZwvCVSbPqlFAH@containers-us-west-32.railway.app:7290/railway'
 
 const pool = new Pool({
   connectionString
-  //  database: 'todo4',
-  //  user:     'postgres',
-  //  password: ''
  })
  
  
