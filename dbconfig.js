6c8babfa22cbaa81e699adbfe09df97a87f2d864
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2023-02-14 15:53:25 -0500

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg')
 
+const connectionLocal = {
+  user: 'postgres',
+  host: 'localhost',
+  database: 'railway',
+  password: 'postgres',
+  port: 5432,
+}
+
 const connectionString = 'postgresql://postgres:Hf6fdE6UzFYbkL44NBb9@containers-us-west-118.railway.app:6551/railway'
 
-const pool = new Pool({
-  connectionString
- })
- 
- 
+const connectionProduction = {
+  connectionString: connectionString
+}
+console.log("Fire Lord Zuko")
+console.log("PORT", process.env.PORT)
+console.log("NODE_ENV", process.env.NODE_ENV)
+const pool = new Pool(process.env.NODE_ENV === 'production' ? connectionProduction : connectionLocal) // This makes it so fellows don't have to change the Pool every time they push to GitHub.
+
  
  module.exports = pool 
